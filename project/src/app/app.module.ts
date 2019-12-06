1b59c258081fbac4b3a9bd63765a2573973b2f0c
giselromina <giselromina@gmail.com>
2019-12-06 11:23:52 +0100

    create componentes

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,20 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { NgModule, Injectable } from '@angular/core';
 
-import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { HttpClientModule } from '@angular/common/http';
+import { GestureConfig } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+//import { BlockUIModule } from 'ng-block-ui';
+import 'hammerjs';
+import { HttpInterceptorProviders } from './core/interceptors';
 
+
+declare var Hammer: any;
+@Injectable()
+export class HammerConfig extends GestureConfig {
+  buildHammer(element: HTMLElement) {
+    return new GestureConfig({ touchAction: 'pan-y' }).buildHammer(element);
+  }
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    HttpClientModule,
+    MatSnackBarModule,
   ],
-  providers: [],
+  providers: [{
+    provide: HAMMER_GESTURE_CONFIG,
+    useClass: HammerConfig
+  },
+    HttpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
